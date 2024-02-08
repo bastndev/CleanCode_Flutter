fd4141f6e4abd8d0d571535fe47b048b0af1c1cf
bastndev <gohit24@gmail.com>
2024-02-08 09:34:29 -0500

    :arrow_up: Update `new 2.0`

--- lib/src/presentation/pages/auth/login/login_page.dart
+++ lib/src/presentation/pages/auth/login/login_page.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          
+          ClipPath(
+            clipper: WaveClipperTwo(),
+            child: Container(
+              height: 200,
+              color: Colors.blue,
+              child: const Row(
+                children: [],
+              ),
+            ),
+          )
         ],
       ),
     );
