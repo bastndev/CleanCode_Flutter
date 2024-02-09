69771a86cf3b731eb6f33cce2c2ee0c3c92f49e2
bastndev <gohit24@gmail.com>
2024-02-09 12:41:57 -0500

    :arrow_up: Update `new 2.0`

--- lib/src/presentation/pages/auth/login/login_page.dart
+++ lib/src/presentation/pages/auth/login/login_page.dart
@@ -1,101 +1,19 @@
+import 'package:clean_code/src/presentation/pages/auth/login/widgets/login_content.dart';
 import 'package:clean_code/src/presentation/utils/base_color.dart';
-import 'package:clean_code/src/presentation/widgets/default_button.dart';
+// import 'package:clean_code/src/presentation/widgets/default_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+// import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
-import '../../../widgets/defauly_textfield.dart';
+// import '../../../widgets/defauly_textfield.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: BACKGROUND_COLOR,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          ClipPath(
-            clipper: WaveClipperTwo(),
-            child: Container(
-              height: MediaQuery.of(context).size.height * 0.35,
-              color: BASE_COLOR,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Image.asset(
-                    'assets/img/gamepad.png',
-                    height: 100,
-                    width: 150,
-                  ),
-                  const Text(
-                    'Game Store',
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 15),
-            child: const Text(
-              'Continue with',
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.only(left: 15),
-            child: const Text(
-              'Log in',
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          const Spacer(),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: DefaultTextField(
-              icon: Icons.email_outlined,
-              label: 'Email',
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            child: DefaultTextField(
-              icon: Icons.lock_outline,
-              label: 'Password',
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(
-              horizontal: 15,
-              vertical: 30,
-            ),
-            width: double.infinity,
-            child: DefaultButton(
-              onPressed: () {},
-              text: 'Log In',
-            ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            margin: const EdgeInsets.only(bottom: 95),
-            child: const Text(
-              'Create an account',
-              style: TextStyle(color: Colors.grey, fontSize: 18),
-            ),
-          )
-        ],
-      ),
+      body: LoginContent(),
     );
   }
 }
