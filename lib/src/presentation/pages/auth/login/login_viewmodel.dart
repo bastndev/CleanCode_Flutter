d1c01000899951249031d82b4be0da59f499f18b
bastndev <gohit24@gmail.com>
2024-02-12 12:26:33 -0500

    :arrow_up: Update `new 2.0`

--- lib/src/presentation/pages/auth/login/login_viewmodel.dart
+++ lib/src/presentation/pages/auth/login/login_viewmodel.dart
@@ -1,6 +1,8 @@
-import 'package:clean_code/src/presentation/pages/auth/login/login_state.dart';
-import 'package:clean_code/src/presentation/utils/validation_item.dart';
+// ignore_for_file: avoid_print 
+//-TODO: provider is other name
 import 'package:flutter/material.dart';
+import 'package:clean_code/src/presentation/utils/validation_item.dart';
+import 'package:clean_code/src/presentation/pages/auth/login/login_state.dart';
 
 class LoginViewModel extends ChangeNotifier {
   LoginState _state = LoginState();
@@ -11,5 +13,16 @@ class LoginViewModel extends ChangeNotifier {
   //SETTERS
   void changeEmail(String value) {
     _state = _state.copyWith(email: ValidationItem(value: value));
+    notifyListeners();
+  }
+
+  void changePassword(String value) {
+    _state = _state.copyWith(password: ValidationItem(value: value));
+    notifyListeners();
+  }
+
+  void login(){
+    print('Email: ${_state.email}');
+    print('Password: ${_state.password}');
   }
 }
