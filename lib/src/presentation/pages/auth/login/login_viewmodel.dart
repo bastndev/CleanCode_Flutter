ba3f474978300f93b6495cd3bd7af1e20bb6e989
bastndev <gohit24@gmail.com>
2024-02-14 09:55:00 -0500

    :arrow_up: Update `new 2.0`

--- lib/src/presentation/pages/auth/login/login_viewmodel.dart
+++ lib/src/presentation/pages/auth/login/login_viewmodel.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: avoid_print 
+// ignore_for_file: avoid_print
 //-TODO: provider is other name
 import 'package:flutter/material.dart';
 import 'package:clean_code/src/presentation/utils/validation_item.dart';
@@ -11,17 +11,34 @@ class LoginViewModel extends ChangeNotifier {
   LoginState get state => _state;
 
   //SETTERS
+  bool isValid() {
+    if (_state.email.value.isEmpty && _state.password.value.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+
   void changeEmail(String value) {
-    _state = _state.copyWith(email: ValidationItem(value: value));
+    if (value.length >= 3) {
+      _state = _state.copyWith(email: ValidationItem(value: value));
+    } else {
+      _state = _state.copyWith(
+          email: const ValidationItem(error: 'Min length is 3'));
+    }
     notifyListeners();
   }
 
   void changePassword(String value) {
-    _state = _state.copyWith(password: ValidationItem(value: value));
+    if (value.length >= 6) {
+      _state = _state.copyWith(password: ValidationItem(value: value));
+    } else {
+      _state = _state.copyWith(
+          password: const ValidationItem(error: 'Min length is 6'));
+    }
     notifyListeners();
   }
 
-  void login(){
+  void login() {
     print('Email: ${_state.email.value}');
     print('Password: ${_state.password.value}');
   }
