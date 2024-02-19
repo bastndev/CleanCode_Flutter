aa785208ae0e2adebdffb2f8ace36d16846f9b3e
bastndev <gohit24@gmail.com>
2024-02-19 10:43:53 -0500

    :arrow_up: Update `add appbar green `

--- lib/src/presentation/pages/auth/register/widgets/register_content.dart
+++ lib/src/presentation/pages/auth/register/widgets/register_content.dart
@@ -1,5 +1,7 @@
-import 'package:clean_code/src/presentation/utils/base_color.dart';
+import 'package:clean_code/src/presentation/widgets/defauly_textfield.dart';
 import 'package:flutter/material.dart';
+
+import 'package:clean_code/src/presentation/utils/base_color.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class RegisterContent extends StatelessWidget {
@@ -8,21 +10,58 @@ class RegisterContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         ClipPath(
           clipper: WaveClipperTwo(),
           child: Container(
-            height: MediaQuery.of(context).size.height * 0.22,
+            height: MediaQuery.of(context).size.height * 0.27,
             color: BASE_COLOR,
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Image.asset(
                   'assets/img/gamepad.png',
                   height: 100,
                   width: 100,
-                )
+                ),
+                const Text(
+                  'Game Store',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
               ],
             ),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 15),
+          child: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                'Continue with',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                'Log up',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 15),
+          child: DefaultTextField(
+            icon: Icons.people,
+            label: 'User name',
+            onChange: (value) {},
           ),
         )
       ],
